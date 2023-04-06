c7dc66b0832449a9958f5cff92e0137cc188ea1b
brianmchugh <brianmchugh02@gmail.com>
2023-04-05 21:23:15 -0400

    Created main script file

---
+++ Main.py
@@ -0,0 +1,7 @@
+# Segmentation Project Main script
+# We keep the main file in def main so that when it only runs when it is called
+def main():
+    # Add your code here
+    print("Hello Friends :)")
+if __name__ == '__main__':
+    main()
