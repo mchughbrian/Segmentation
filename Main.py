4209c4e88fefe3fa22b6c48877230401ff37f9e2
brianmchugh <brianmchugh02@gmail.com>
2023-04-09 08:46:19 -0400

    Added def_image_path() function

--- Main.py
+++ Main.py
@@ -2,6 +2,21 @@
 # We keep the main file in def main so that when it only runs when it is called
 import os
 from PIL import Image
+from tkinter import filedialog
+
+
+def get_image_path():
+    # Prompt user to select an image file
+    root = Tk()
+    root.withdraw() # hide the Tkinter root window
+    file_path = filedialog.askopenfilename(title="Select Image",
+                                           filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp")])
+    # Check if file format is supported
+    _, ext = os.path.splitext(file_path)
+    if ext.lower() not in SUPPORTED_FORMATS:
+        raise ValueError(f"Unsupported file format: {ext}")
+    return file_path
+
 
 def main():
     # Add your code here
