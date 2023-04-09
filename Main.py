08f0b9b4fc9e7ee07e2c1c5e93700c0ee2f26f70
brianmchugh <brianmchugh02@gmail.com>
2023-04-09 08:35:43 -0400

    Added Pillow library and imported

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 # Segmentation Project Main script
 # We keep the main file in def main so that when it only runs when it is called
+import os
+from PIL import Image
+
 def main():
     # Add your code here
     print("Hello Friends :)")
